7bab2b9350b7d33359156e61deff76200410eb5c
Julia Naomi <jnboeira@outlook.com>
2020-02-02 01:17:25 -0300

    uses features to test db

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 db:
 	docker run -p 8000:8000 amazon/dynamodb-local
+
+test:
+	cargo teste --features "dynamo"
--- src/todo_api/db/todo.rs
+++ src/todo_api/db/todo.rs
@@ -1,11 +1,12 @@
-use crate::todo_api::{
-    db::helpers::{client, TODO_CARD_TABLE},
-    model::TodoCardDb,
-};
-use rusoto_dynamodb::{DynamoDb, PutItemInput};
+use crate::todo_api::{db::helpers::TODO_CARD_TABLE, model::TodoCardDb};
+use rusoto_dynamodb::PutItemInput;
 use uuid::Uuid;
 
+#[cfg(not(feature = "dynamo"))]
 pub fn put_todo(todo_card: TodoCardDb) -> Option<Uuid> {
+    use crate::todo_api::db::helpers::client;
+    use rusoto_dynamodb::DynamoDb;
+
     let client = client();
     let put_item = PutItemInput {
         table_name: TODO_CARD_TABLE.to_string(),
@@ -17,4 +18,14 @@ pub fn put_todo(todo_card: TodoCardDb) -> Option<Uuid> {
         Ok(_) => Some(todo_card.id),
         Err(_) => None,
     }
+}
+
+#[cfg(feature = "dynamo")]
+pub fn put_todo(todo_card: TodoCardDb) -> Option<Uuid> {
+    let _ = PutItemInput {
+        table_name: TODO_CARD_TABLE.to_string(),
+        item: todo_card.clone().into(),
+        ..PutItemInput::default()
+    };
+    Some(todo_card.id)
 }
